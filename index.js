0e80ffb18c9e33077a61dc7dcc3ec353ce8a2e03
LHardyWilcox <motacycaryda@mac.com>
2023-03-01 15:26:17 -0800

    added table of contents

--- index.js
+++ index.js
@@ -5,55 +5,55 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions =[
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is your project title?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Describe your project.',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'What is required to install your project?',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Describe how to use your project.',
-        },
-        {
-            type: 'input',
-            name: 'credits',
-            message: 'What resources or collaborations did you use to build this project?',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What type of licese does your project have?',
-            choices: ['GNU GPLv3', 'MIT', 'Boost Software License 1.0'],
-        },
-         {
-            type: 'input',
-            name: 'tests',
-            message: 'What tests have been developed for this project?',
-        },
-         {
-            type: 'input',
-            name: 'github',
-            message: 'What is the address of your github account?',
-        },
-         {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?',
-        }
-    ];
-  
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What is required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Describe how to use your project.',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'What resources or collaborations did you use to build this project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of licese does your project have?',
+        choices: ['GNU GPLv3', 'MIT', 'Boost Software License 1.0'],
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What tests have been developed for this project?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the address of your github account?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }
+];
+
 
 // TODO: Create a function to write README file
 
@@ -66,7 +66,7 @@ function writeReadMe(data) {
             console.log('README created!');
         }
     })
-  
+
 };
 
 // TODO: Create a function to initialize app
@@ -74,11 +74,11 @@ function writeReadMe(data) {
 // What needs to be initialized? Am I just calling functions here?  Is this where I link the generateMarkdown.js file?
 function init() {
     inquirer.prompt(questions)
-    .then(answers => {
-        //How do I get these answers over to the generateMarkdown.js?
-        const markdown = generateMarkdown(answers);
-        writeReadMe(markdown);
-    })
+        .then(answers => {
+            //How do I get these answers over to the generateMarkdown.js?
+            const markdown = generateMarkdown(answers);
+            writeReadMe(markdown);
+        })
 }
 
 
